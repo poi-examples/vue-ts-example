1d071843b1e87a22ad575f7ff5601c22c3ea4492
EGOIST <0x142857@gmail.com>
2018-04-19 15:38:04 +0800

    update poi.config.ts

--- poi.config.ts
+++ poi.config.ts
@@ -1,6 +1,10 @@
-export default {
+import { Options } from 'poi'
+
+const options: Options = {
   entry: 'src/index.ts',
   plugins: [
     require('@poi/plugin-typescript')()
   ]
 }
+
+export default options
